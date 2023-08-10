644638131f6c3805c3c025586b1792e8ef981ea4
MarkoR-19 <marko.roza19@gmail>
2023-08-10 19:37:43 +0200

    Changed ws to wss in routing.py

--- vrticconnect/routing.py
+++ vrticconnect/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/vrticconnect/(?P<grupa>\w+)/$',consumers.grupaConsumer.as_asgi()),
+    re_path(r'wss/vrticconnect/(?P<grupa>\w+)/$',consumers.grupaConsumer.as_asgi()),
 ]
