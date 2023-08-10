c474ee97bcc13bf41f36323a953d3d17a484d2b1
MarkoR-19 <marko.roza19@gmail>
2023-08-10 18:57:09 +0200

    Changed settings-py

--- diplomski/settings.py
+++ diplomski/settings.py
@@ -150,6 +150,7 @@ if STORAGE_DESTINATION == 's3':
     ]
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATIC_ROOT = 'static'
     
     # public media settings
     PUBLIC_MEDIA_LOCATION = 'media'
